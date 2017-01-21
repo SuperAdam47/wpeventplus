e2a4c41b8bf0dcd30f39f2b3fa678ad834dc75a9
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2017-01-21 21:17:15 +0400

    Stripe Warnings added

--- eventplus/app/views/admin/settings/tabs/payment_fields/stripeactive.php
+++ eventplus/app/views/admin/settings/tabs/payment_fields/stripeactive.php
@@ -1,4 +1,3 @@
-<?php /* * *******************STRIPE API KEY******************** */ ?>
 
 <p>
     <label for="payment_vendor_id">
@@ -14,7 +13,7 @@
     <br />
     <input class="regular-text" type="text" value="<?php echo $company_options['publishable_key']; ?>" size="60" name="publishable_key">
 </p>
-<?php /* * ****************stripe return url ****** */ ?>
+
 <p style="display:none;">
     <label for="stripereturn_url">
         <?php _e('Stripe Return Url', 'evrplus_language'); ?>
@@ -23,4 +22,30 @@
     <input class="regular-text" type="text" value="<?php echo $company_options['stripereturn_url']; ?>" size="60" name="stripereturn_url">
 </p>
 
-<?php /* * ****************stripe return url ****** */ ?>
+
+<?php
+$stripeRequirementNotices = array();
+if (!function_exists('curl_init')) {
+    $stripeRequirementNotices[] = 'curl_init: Stripe needs the CURL PHP extension.';
+}
+if (!function_exists('json_decode')) {
+    $stripeRequirementNotices[] = 'json_decode: Stripe needs the JSON PHP extension.';
+}
+if (!function_exists('mb_detect_encoding')) {
+    $stripeRequirementNotices[] = 'mb_detect_encoding: Stripe needs the Multibyte String PHP extension.';
+}
+
+if (count($stripeRequirementNotices)):
+    ?>
+    <p>
+        <label for="stripereturn_warning" style="color:red;">
+            <?php _e('Warning! Stripe will not work as intended. Please activate following PHP extensions:', 'evrplus_language'); ?>
+        </label>
+        <br />
+    <ul>
+        <?php foreach ($stripeRequirementNotices as $k => $stripeRequirementNotice): ?>
+            <li><?php echo $stripeRequirementNotice; ?></li>
+        <?php endforeach; ?>
+    </ul>
+    </p>
+<?php endif; ?>
