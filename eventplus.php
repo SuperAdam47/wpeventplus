cde2ab7d3a01b4a757b56514a67026fe7dbbc597
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2017-04-07 21:00:52 +0400

    Version change

--- eventplus.php
+++ eventplus.php
@@ -2,7 +2,7 @@
 
 /** Plugin Name: WP EventsPlus
  * Description: Events Plus allows you to easily create and manage your events. Allow visitors to register and pay online for events, manage attendees, discount coupons, export attendees list, and much more.
- * Version: 2.2.4
+ * Version: 2.2.5
  * Author: wpeventsplus.com
  * Author URI: http://wpeventsplus.com/
  * License: GPL2
