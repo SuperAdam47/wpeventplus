63f349f9a2a84eb542f641a5e43247439953a05c
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2017-07-21 12:19:08 +0530

    Resolve questions single / radio field type not save to DB.

--- eventplus/app/views/front/event/parts/inc/form_fields/radio.php
+++ eventplus/app/views/front/event/parts/inc/form_fields/radio.php
@@ -3,6 +3,5 @@
 
 foreach ($values as $key => $value) {
     $checked = in_array($value, $answers) ? ' checked="checked"' : "";
-    echo '<label class="radi0"><input title="'.$question->question.'" class="'.$requiredClass.'" type="radio" name="MULTIPLE_'.$question->id.'[]" value="'.$value.'" '.$checked.'> '.$value.'</label>';
+    echo '<label class="radi0"><input title="'.$question->question.'" class="'.$requiredClass.'" type="radio" id="'.$value.'" name="SINGLE_'.$question->id.'" value="'.$value.'" '.$checked.'> '.$value.'</label>';
 }
-                
