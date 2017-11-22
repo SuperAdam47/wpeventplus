729733e6fb0b7db07cd6d9b21bda02e86df2c441
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2017-11-22 10:22:14 +0530

    Resolve video not working in event description. ticket: 1374823

--- eventplus/app/views/front/event/parts/regform.php
+++ eventplus/app/views/front/event/parts/regform.php
@@ -174,7 +174,9 @@ if (isset($event_meta_data)) {
                 </div>
 
                 <div class="d3sc" id="eventplus_event_description">
-                    <p><?php echo html_entity_decode(nl2br($event_desc)); ?></p>
+                    <p><?php
+                    $event_desc = apply_filters( 'the_content', $event_desc );
+                    echo html_entity_decode(nl2br($event_desc)); ?></p>
                 </div>
 
                 <?php if ($google_map == "Y"): ?>
