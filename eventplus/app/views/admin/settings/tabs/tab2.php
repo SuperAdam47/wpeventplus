8649ea0e1164dc9ff9f26b9961f0f47faab54833
SuperAdam47 <hermelindo.lopez0714@gmail.com>
2018-05-05 10:30:17 +0530

    Added UGX - Uganda Shillings Currency

--- eventplus/app/views/admin/settings/tabs/tab2.php
+++ eventplus/app/views/admin/settings/tabs/tab2.php
@@ -58,6 +58,7 @@ $paymentMethods = EventPlus_Models_Payments::getPaymentMethods();
                             <option value="SAR" <?php if ($company_options['default_currency'] == 'SAR') echo ' selected'; ?>>SAR</option>
                             <option value="PHP" <?php if ($company_options['default_currency'] == 'PHP') echo ' selected'; ?>>PHP</option>
                             <option value="INR" <?php if ($company_options['default_currency'] == 'INR') echo ' selected'; ?>>INR</option>
+                            <option value="UGX" <?php if ($company_options['default_currency'] == 'UGX') echo ' selected'; ?>>UGX</option>
                         </select>
                     </div>
                     </p> <br />
